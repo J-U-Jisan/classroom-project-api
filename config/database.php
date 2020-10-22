0d46e14fb96c616be059e5f323d5bd7bc506fff8
J-U-Jisan <jisan.cse16@gmail.com>
2020-10-23 00:18:18 +0600

    postgresql

--- config/database.php
+++ config/database.php
@@ -1,19 +1,19 @@
 <?php
 class Database{
      // specify your own database credentials
-    private $host = "localhost";
-    private $db_name = "classroom_api";
-    private $username = "root";
-    private $password = "";
+    private $host = "ec2-54-246-115-40.eu-west-1.compute.amazonaws.com";
+    private $db_name = "d9kedkfr6qjllu";
+    private $username = "pgtjvvlriwlswi";
+    private $password = "f4dd00d2ba544386ff442844ad6e1c6849f0e7a7450f2e0c660a8d062bd75f04";
     public $conn;
  
     // get the database connection
     public function getConnection(){
  
         $this->conn = null;
- 
+        $dsn = "pgsql:host=$host;port=5432;dbname=$db_name;user=$username;password=$password";
         try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO($dsn);
             $this->conn->exec("set names utf8");
         }catch(PDOException $exception){
             echo "Connection error: " . $exception->getMessage();
