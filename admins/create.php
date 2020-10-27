01a00e68aa203bc16c5d015b41a25115ecb2768a
J-U-Jisan <jisan.cse16@gmail.com>
2020-10-28 00:50:12 +0600

    header2

--- admins/create.php
+++ admins/create.php
@@ -3,11 +3,10 @@
     // required headers
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
-    header("X-HTTP-Method-Override: PATCH");
+
     header("Access-Control-Allow-Methods: POST");
     header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    header("Authorization: cce326abfc384ef5a674f11e5587ede1");
     // get database connection
     include_once '../config/database.php';
      
